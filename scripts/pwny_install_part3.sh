e2a7bfd54f55fe387c7bb7de2d412d13eb3c214e
V0rT3x <70115207+V0r-T3x@users.noreply.github.com>
2023-05-12 18:10:43 -0400

    Create pwny_install_part3.sh

---
+++ scripts/pwny_install_part3.sh
@@ -0,0 +1,30 @@
+### Install requirements.txt followed by the Pwnagotchi
+cd Pwnagotchi-For-Banana-Orange-Pi/
+TMPDIR=/root/tmp python3.7 -m pip install -r requirements.txt
+TMPDIR=/root/tmp python3.7 -m pip install .
+
+## Fix permissions errors
+
+sudo chmod 755 /usr/bin/bettercap
+sudo chown root:root /usr/bin/bettercap
+sudo chmod 755 /usr/bin/bettercap-launcher
+sudo chmod 755 /usr/bin/pwngrid
+sudo chown root:root /usr/bin/pwngrid
+sudo chmod 755 /usr/local/bin/pwnagotchi
+sudo chown root:root /usr/local/bin/pwnagotchi
+sudo chmod 711 /usr/bin/pwnagotchi-launcher
+sudo chmod +x /usr/bin/monstart
+sudo chmod +x /usr/bin/monstop
+sudo chmod +x /etc/systemd/system/pwnagotchi.service
+sudo chmod +x /etc/systemd/system/pwngrid-peer.service
+
+
+############ edit pwnlib and change interface mon0 command to below
+############ wlanxxxxxx should be the name of the external wifi device in iwconfig eg wlx00ee11ff00cc 
+#wlxe84e065ce1f5
+#iwconfig
+#iw phy `iw dev wlxe84e065ce1f5 info | gawk '/wiphy/ {printf "phy" $2}'` interface add mon1 type monitor
+##### copy iface name and change wlanxxxxxx to that device name and change command in /usr/bin/pwnlib
+#iw phy `iw dev wlanxxxxxx info | gawk '/wiphy/ {printf "phy" $2}'` interface add mon0 type monitor
+
+sudo systemctl enable bettercap pwngrid-peer pwnagotchi
